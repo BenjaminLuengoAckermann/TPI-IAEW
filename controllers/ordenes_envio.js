377791aad2c712f9fefcb8e7fc1cd5530e9233ee
BenjaminLuengoAckermann <benjaluengoa@gmail.com>
2022-11-16 22:45:53 -0300

    webhook

--- controllers/ordenes_envio.js
+++ controllers/ordenes_envio.js
@@ -4,6 +4,7 @@ const Op = Sequelize.Op;
 const ordenes_envio = require('../models').ShippingOrders;
 const shipping_status = require('../models').ShippingStatus;
 const deliveries = require("../models").Deliveries
+const axios = require("axios");
 
 module.exports = {
 
@@ -103,7 +104,7 @@ module.exports = {
 
         const orderResponse = ordenes_envio.findOne({
             where: {
-                orderId: req.params.orderId, 
+                orderId: req.params.orderId,
                 shippingStatus: 2
             }
         })
@@ -126,6 +127,7 @@ module.exports = {
                     Promise
                         .all([desocuparReparto])
                         .then(response => {
+                            // Actualiza el estado de la orden de envio
                             return ordenes_envio
                                 .update({
                                     shippingStatus: 3,
@@ -137,7 +139,43 @@ module.exports = {
                                         }
                                     },
                                 )
-                                .then(delivery => res.status(200).send(delivery))
+                                .then(delivery => {
+                                    // Notificacion via webhook al modulo API Procesador de Envios
+                                    // POST /envios/{id_envios}/novedades
+                                    const url_auth = "https://dev-pmt16h97.us.auth0.com/oauth/token"
+                                    const url_notification = 'http://ecs-services-1705455222.us-east-1.elb.amazonaws.com/api/Envios/'
+                                    + orderId + '/Novedades'
+                                    const body = {
+                                        client_secret: "6pVQtQwSKefZ_4N6hTbo17NHfazaLcyp3gF1GqGLefl64eC_zn8RDrYuHvaYlWrH",
+                                        client_id: "C0L2So2k7ZBdKwhMCWwRZCZQYHqThX7a",
+                                        grant_type: "client_credentials",
+                                        audience: "https://api.procesadorenvios.com"
+                                    }
+                                    // Primero nos autenticamos en el servidor
+                                    axios.post(url_auth, body, {
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        }
+                                    }).then((token) => {
+                                        // Si recibimos el token notificamos el cambio de estado del envio
+                                        let body_notification = {delivery: delivery}
+                                        axios.post(url_notification, body_notification, {
+                                            headers: {"Content-Type": "application/json",
+                                            Authorization: 'Bearer ' + token}
+                                        }).then((response) => {
+                                            return res.status(200).send(delivery)
+
+                                        })
+                                        .catch((error) => {
+                                            return res.status(400).send(error)
+                                        })
+                                    })
+                                    .catch((error) => {
+                                        return res.status(400).send(error)
+                                    });
+                                    
+                                    res.status(200).send(delivery)
+                                })
                                 .catch(error => res.status(400).send(error))
                         })
 
